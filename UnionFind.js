4d77f469675957220c3e92e0b05b1d50d94a85e0
Akshay Khanna <akshay9911102163@gmail.com>
2022-04-16 09:08:48 +0530

    Update UnionFind.js

--- UnionFind.js
+++ UnionFind.js
@@ -1,3 +1,12 @@
+/* 
+The main idea of a “disjoint set” is to have all connected vertices have the same parent node or root node, whether directly or indirectly connected. 
+To check if two vertices are connected, we only need to check if they have the same root node.
+
+The two most important functions for the “disjoint set” data structure are the find function and the union function. 
+The find function locates the root node of a given vertex. The union function connects two previously unconnected vertices by giving them 
+the same root node. There is another important function named connected, which checks the “connectivity” of two vertices. 
+The find and union functions are essential for any question that uses the “disjoint set” data structure.
+*/
 class UnionFind {
   constructor(size) {
     // would be storing parent
