c2c635964b8070a01cd5990cce54fbb98913d761
akshay.khanna <akshay.khanna@servicenow.com>
2022-08-02 20:27:33 +0530

    Fib with DP

--- DP/fibonacci.js
+++ DP/fibonacci.js
@@ -1,7 +1,10 @@
-function fibonacciNo(n) {
+function fibonacciNo(n, memo = {}) {
+  if (n in memo) return memo[n];
   if (n <= 2) return 1;
-  return fibonacciNo(n - 1) + fibonacciNo(n - 2);
+  memo[n] = fibonacciNo(n - 1, memo) + fibonacciNo(n - 2, memo);
+  return memo[n];
 }
+
 console.log(fibonacciNo(2));
 console.log(fibonacciNo(6));
 console.log(fibonacciNo(7));
