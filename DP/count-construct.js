2ea09cd9dd27d5a035f2287059bb8152d2c7de88
akshay.khanna <akshay.khanna@servicenow.com>
2022-08-21 08:12:44 +0530

    count construct

---
+++ DP/count-construct.js
@@ -0,0 +1,29 @@
+function countConstruct(targetString, words, memo = {}) {
+  if (targetString in memo) return memo[targetString];
+  if (targetString === "") return 1;
+  let count = 0;
+  for (let word of words) {
+    if (targetString.substr(0, word.length) === word) {
+      count += countConstruct(targetString.substr(word.length), words, memo);
+    }
+  }
+  memo[targetString] = count;
+  return count;
+}
+
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // 1
+console.log(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"])); // 2
+console.log(
+  countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+); // 0
+console.log(countConstruct("", ["cat", "dog", "mouse"])); // 1
+// after memoization it will work instanly
+console.log(
+  countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+  ])
+); // false
